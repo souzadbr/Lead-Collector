8de5fdc1e00fdbbf489490919ba8c31ae1958f32
Debora Souza <deboral.p@hotmail.com>
2021-12-21 14:05:39 -0300

    Implementar método de salvar lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -1,2 +1,14 @@
-package br.com.zup.LeadCollector.lead;public class LeadService {
+package br.com.zup.LeadCollector.lead;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeadService {
+    @Autowired
+    private LeadRepository leadRepository;
+
+    public Lead salvarLead(Lead lead){
+        return leadRepository.save(lead);
+    }
 }
