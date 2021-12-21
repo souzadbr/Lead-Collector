c139e5fc0f27171edc532b0a566ae7829a1d8c33
Debora Souza <deboral.p@hotmail.com>
2021-12-21 11:09:08 -0300

    Modelar classe de entidade Produto

--- src/main/java/br/com/zup/LeadCollector/Produto.java
+++ src/main/java/br/com/zup/LeadCollector/Produto.java
@@ -1,4 +1,33 @@
 package br.com.zup.LeadCollector;
 
+import javax.persistence.*;
+
+@Entity //entidade gera tabela no banco de dados
+@Table (name = "produtos") //dá nome para tabela ou pode ser usado apra mapear uma tabela que já existe.
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)//esse nome será unico, somente um produto com mesmo nome
+    private String nome;
+
+    //Por padrão se não tem cosntrutor, já tenho um construtor vazio
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
