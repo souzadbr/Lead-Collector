5f83eaf7f1b6d7ef0a68e49021c8f2e2aff7e591
Debora Souza <deboral.p@hotmail.com>
2021-12-21 17:42:34 -0300

    Adicionar query na interface combanco de dados

--- src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 //Essa interface faz ligação com o banco de dados -  Produto e sua chave primaria no <>
 
 public interface ProdutoRepository extends CrudRepository<Produto,Integer> {
-    boolean existByNome(String nome); //vai verificar se o produto já existe cadastrado no banco de dados e retorna
+    boolean existByNome(String nome);
+
+    Produto findBynome(String nome);
+
+    //vai verificar se o produto já existe cadastrado no banco de dados e retorna
     // sim ou não
 }
