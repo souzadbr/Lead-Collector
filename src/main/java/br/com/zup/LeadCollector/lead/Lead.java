e565d82353b32e6e0d489efe2fb47e363acf415f
Debora Souza <deboral.p@hotmail.com>
2021-12-21 13:50:50 -0300

    Modelar classe entidade lead

--- src/main/java/br/com/zup/LeadCollector/lead/Lead.java
+++ src/main/java/br/com/zup/LeadCollector/lead/Lead.java
@@ -1,2 +1,39 @@
-package br.com.zup.LeadCollector.lead;public class Lead {
+package br.com.zup.LeadCollector.lead;
+
+import br.com.zup.LeadCollector.produto.Produto;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name= "leads")
+public class Lead {
+    private String email;
+    private String nome;
+    private List<Produto>produtosDeInteresse;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getProdutosDeInteresse() {
+        return produtosDeInteresse;
+    }
+
+    public void setProdutosDeInteresse(List<Produto> produtosDeInteresse) {
+        this.produtosDeInteresse = produtosDeInteresse;
+    }
 }
