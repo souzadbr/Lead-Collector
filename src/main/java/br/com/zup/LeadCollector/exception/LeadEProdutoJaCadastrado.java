172a557cc8354399d72d1303dae021fef8cd2531
Debora Souza <deboral.p@hotmail.com>
2021-12-22 09:00:54 -0300

    Criar exception persopnalizada

--- src/main/java/br/com/zup/LeadCollector/exception/LeadEProdutoJaCadastrado.java
+++ src/main/java/br/com/zup/LeadCollector/exception/LeadEProdutoJaCadastrado.java
@@ -1,2 +1,7 @@
-package br.com.zup.LeadCollector.exception;public class LeadEProdutoJaCadastrado {
+package br.com.zup.LeadCollector.exception;
+
+public class LeadEProdutoJaCadastrado extends  RuntimeException{
+    public LeadEProdutoJaCadastrado(String message) {
+        super(message);
+    }
 }
