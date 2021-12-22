355991e66f8da7962566d9ae26fa8f4ee5f76d87
Debora Souza <deboral.p@hotmail.com>
2021-12-22 09:00:11 -0300

    Adicionar query na Produtorepository

--- src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/LeadCollector/produto/ProdutoRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 //Essa interface faz ligação com o banco de dados -  Produto e sua chave primaria no <>
 
 public interface ProdutoRepository extends CrudRepository<Produto,Integer> {
-    boolean existByNome(String nome);
+    boolean existsByNome(String nome);
 
-    Produto findBynome(String nome);
+    Produto findByNome(String nome);
 
     //vai verificar se o produto já existe cadastrado no banco de dados e retorna
     // sim ou não
