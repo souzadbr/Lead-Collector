bb1c496cf649d4ff025b4bf08e5806700f28dcc2
Debora Souza <deboral.p@hotmail.com>
2021-12-22 09:13:09 -0300

    Criar classe LeadController

---
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
@@ -0,0 +1,16 @@
+package br.com.zup.LeadCollector.lead;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RequestMapping("/leads")
+@RestControllerAdvice
+public class LeadController {
+    @Autowired
+    private LeadService leadService;
+
+
+}
