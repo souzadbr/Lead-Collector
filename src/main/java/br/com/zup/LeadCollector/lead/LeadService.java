8a30586cc74c3c3c6267d88bc466fb854184a0f4
Debora Souza <deboral.p@hotmail.com>
2021-12-22 09:02:11 -0300

    Criar metodo de buscar produto

--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -1,14 +1,42 @@
 package br.com.zup.LeadCollector.lead;
 
+
+import br.com.zup.LeadCollector.produto.Produto;
+import br.com.zup.LeadCollector.produto.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class LeadService {
-    @Autowired
     private LeadRepository leadRepository;
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    public LeadService(LeadRepository leadRepository, ProdutoRepository produtoRepository) {
+        this.leadRepository = leadRepository;
+        this.produtoRepository = produtoRepository;
+    }
 
     public Lead salvarLead(Lead lead){
         return leadRepository.save(lead);
     }
+
+    public List<Produto> buscarProdutos(List<Produto> produtos) {
+        List<Produto> listaAtualizada = new ArrayList<>();
+
+        for (Produto produto : produtos) {
+            if (produtoRepository.existsByNome(produto.getNome())) {
+                listaAtualizada.add(produtoRepository.findByNome(produto.getNome()));
+            }else {
+                listaAtualizada.add(produto);
+            }
+        }
+
+        return listaAtualizada;
+    }
+
+
 }
